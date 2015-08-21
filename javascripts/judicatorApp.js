891b26586c86f3993f054d62f68befd99848d42f
Matthew <mattemhotep@gmail.com>
2015-08-21 20:45:54 +0800

    Added reg form. Made reg controller sa angular. DO backend at some point. Add the DB too

--- javascripts/judicatorApp.js
+++ javascripts/judicatorApp.js
@@ -1,5 +1,21 @@
 var app = angular.module('judicatorApp', []);
 
+app.controller('registrationController', function($scope) {
+    $scope.user = {
+        'username': '',
+        'password' : '',
+        'password2' : '',
+        'firstName': '',
+        'lastName': '',
+        'email': ''
+    };
+    $scope.errMsg = '';
+    $scope.register = function() {
+        // DB QUERY FOR CREDENTIALS
+        alert('USERNAME: ' + $scope.user.username + '\nPASSWORD: ' +  $scope.user.password);
+    };
+});
+
 app.controller('mainController', function($scope) {
     $scope.posts = [];
     $scope.newPost = {
