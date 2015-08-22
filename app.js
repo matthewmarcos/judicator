6402db1fdb27b167705573ea3299738ba3e13b74
Matthew <mattemhotep@gmail.com>
2015-08-22 23:02:25 +0800

    Front end can be hosted with server. Angular-router now in place. Need to hook up angular to DB

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var passport = require('passport');
+var index = require('./routes/index');
 var auth = require('./routes/auth')(passport);
 var mongoose = require('mongoose');
 
@@ -39,14 +40,16 @@ app.use(passport.session());
 var initPassport = require('./passport-init');
 initPassport(passport);
 
+// Routes
+app.use('/', index);
 app.use('/api', api);
 app.use('/auth', auth);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -54,23 +57,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
--- public/javascripts/judicatorApp.js
+++ public/javascripts/judicatorApp.js
@@ -1,4 +1,23 @@
-var app = angular.module('judicatorApp', []);
+var app = angular.module('judicatorApp', ['ngRoute']);
+
+app.config(function($routeProvider){
+  $routeProvider
+    //the timeline display
+    .when('/', {
+      templateUrl: '/angularPartials/main.html',
+      controller: 'mainController'
+    })
+    //the login display
+    .when('/login', {
+      templateUrl: '/angularPartials/login.html',
+      controller: 'authController'
+    })
+    //the signup display
+    .when('/register', {
+      templateUrl: '/angularPartials/register.html',
+      controller: 'authController'
+    });
+});
 
 app.controller('authController', function($scope) {
     $scope.user = {
